28f7968ea712cab8dc07384e0bebdc707452dfc0
kukreja_raghav <raghavkuk25@gmail.com>
2016-06-17 10:52:42 +0530

    Admin login fixed

--- admin/alogin.php
+++ admin/alogin.php
@@ -1,12 +1,12 @@
 <?php
+include '../db_connection.php';
 session_start();
 $an=$_POST["aname"];
 $ap=$_POST["apass"];
 
-$con=mysqli_connect("localhost","root","");
-   mysqli_select_db($con,"tpo");
+   
    $query="SELECT * from admin_login where admin_username='".$an."' and admin_password='".$ap."'";
-   $result = $con->query($query);
+   $result = $mysqli->query($query);
    if($result->num_rows>0)
    {
 	  
@@ -17,5 +17,5 @@ $con=mysqli_connect("localhost","root","");
 	   header('Location: admin-login.php?status=failed');
    }
    $_SESSION['statusadd']='failure';
-$con->close();
+$mysqli->close();
 ?>
